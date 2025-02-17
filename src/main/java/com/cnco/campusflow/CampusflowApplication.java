4c8feebeb4a788d4047a6daf135534e23d94e3ae
hyj2u <hyj2u94@gmail.com>
2025-02-17 09:22:43 +0900

    Initial commit

---
+++ src/main/java/com/cnco/campusflow/CampusflowApplication.java
@@ -0,0 +1,13 @@
+package com.cnco.campusflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cnco/campusflow/CampusflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnco.campusflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
